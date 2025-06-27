900f534c8688cc2732511cad6f2161cbade7e8fe
Engy-Elyamany <engyelyamany26@gmail.com>
2025-06-27 04:10:13 +0300

    Correct Tail Drawing, prevent 180 movement, Updated Randomized Food

--- DEV_LOG.md
+++ DEV_LOG.md
@@ -3,42 +3,58 @@
 
 ### movement and player control
 - The player controls the direction of movement of the snake through keyboard keys (UP, DOWN, LEFT, RIGHT)
+- 180-deg Movement must be prevented (the snake can't move left while it moves right)
 - Keys must be "pressed" (not held down) to change direction - uses IsKeyPressed() instead of IsKeyDown()
 - Each key set the speed position :
-    KEY_RIGHT => speed {x,0}
-    KEY_DOWN => speed {0,y}
+    - KEY_RIGHT => speed {x,0}
+    - KEY_DOWN => speed {0,y}
 - Then for each 4 frames, the snake uses the stored speed coordinates to actually move
 - The snake moves every 4 frames to prevent the snake to move very fast.
 
 ### The snake's body 
 - snake is an array of structs each struct stores rectangle properties and speed
-- the body of the snake is segments.
-- we store the poistion of each segment in snake_prev_pos[] and then uses it 
+- the body of the snake is segments. Which needs to be intialized first off
+- we store the position of each segment in snake_prev_pos[] and then uses it 
 - when head moves to its next position, the following segment takes the previous position of the head
 - What if you have 3 segments?
-    Segment 1 moves to where the head was
-    Segment 2 moves to where segment 1 was
-    Segment 3 moves to where segment 2 was
+    - Segment 1 moves to where the head was
+    - Segment 2 moves to where segment 1 was
+    - Segment 3 moves to where segment 2 was
 
 ### Randomize Food
 - we choose rows and columns randomly through GetRandomValue()
-- then convert them into pixels to assign them to the coordinates of the food rectangle
-- For the result to appear : Food position randomized but fitting completely into grid cells
+- then convert them into pixels and assign them to the coordinates of the food rectangle
+- For the result to appear as Food position randomized but fitting completely into grid cells
+- The food must not intersect with the snake's body :
+    - check if the new coordinates of the food equals any coordinates of the body 
+    - if true, generate new random coordinates and check starting from the head again.
+    - else,proceed to the next segment to check
+    - After the whole length is checked, flag a valid_food_position : true
 
 ### Collision between snake and food 
 - we use CheckCollisionRecs(), to determine if the food and the snake[0]'s head collided or not
-- When collision happens, we set the flag to 1 and increment the length of the snake.
+- When collision happens:
+    -  we set the valid_food_position flag to 0 to indicate the need for the next valid position
+    - Then increment the length of the snake.
+
+### Losing the game
+- 
+- 
+- 
+
 
 <br/>
 
 ## TO DO
-1. make the tail bend 
+1. make the tail bend ✅
 2. start the game running ✅
 3. collision logic ✅
-4. drawing the length of the snake
+4. drawing the length of the snake ✅
 5. When are you losing the game?
 6. Game states
-7. collision with own tail
+7. food position shouldn't intersect with the snake's tail ✅
+8. Score 
+9. Adjust play field, all the controls,...etc
 
 
 ### [ Tuesday 24-6-2025  03:03 ] :
@@ -48,9 +64,19 @@
 ![Snake2025-06-2506-11-14-ezgif com-video-to-gif-converter](https://github.com/user-attachments/assets/721a3121-9585-4763-a028-a39996417b62)
 
 
-### [ Wednesday 25-6-2025  00:00 ] :
+### [ Wednesday 25-6-2025  03:20 ] :
 1. fixed that randomly disappearing food from yesterday
-2. Improved key controls
+2. Improved key controls (changed IsKeyDown to IsKeyPressed)
 3. implemented player movement with speed 
 4. implemented the body logic but there's still a problem with drawing the tail??!
 ![Snake2025-06-2603-16-17-ezgif com-video-to-gif-converter](https://github.com/user-attachments/assets/3d3dbb3a-3103-4701-995f-8b35a0d67e35)
+
+
+### [ Thursday 26-6-2025  03:10 ] :
+1. Now the tail shows (only needed to intialize the body segments)
+2. allowe movement only in one direction (the snake can't move left while it moves right) AKA reversal or 180-deg turn. Acomplished that through checking if the speed in the desired direction is zero
+3. Updated Randomized food logic (now the food will not appear on the sanke's tail (on a position where the snake has segments))
+![Snake2025-06-2704-03-09-ezgif com-video-to-gif-converter](https://github.com/user-attachments/assets/ad144ffd-ff7d-4fda-8398-b1d84c1d131c)
+
+
+
--- main.c
+++ main.c
@@ -15,49 +15,94 @@ typedef struct
 
 } Snake;
 
-Vector2 snake_prev_pos[SNAKE_LENGTH] ;
+Vector2 snake_prev_pos[SNAKE_LENGTH];
 
 // ---------------------------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main()
 {
     InitWindow(screen_width, screen_height, "Snake");
     SetTargetFPS(60);
+    
     int frame_cnt = 0, length = 0;
-   
+    bool valid_food_pos = 1;
 
     // intialize snake
     Snake snake[SNAKE_LENGTH];
-    snake[0].snake_rect = (Rectangle){0, 0, GRID_BOX, GRID_BOX}; // intial position of the snake rect
-    snake[0].speed = (Vector2){GRID_BOX, 0};                     // the intial speed is in +ve X-axis direction
+    // here we intialize all segments of the snake body to be able to store them in the first place
+    //  Then , we use these stored positions to create the flow of the tail
+    //  each 4 frames, we move the head with the stored speed and update the segments accordingly
+    for (int i = 0; i < SNAKE_LENGTH; i++)
+    {
+        // -(i*GRID_BOX) to intialize x coordinate of each segment right behind its previous one
+        snake[i].snake_rect = (Rectangle){-(i * GRID_BOX), 0, GRID_BOX, GRID_BOX};
+    }
+    snake[0].speed = (Vector2){GRID_BOX, 0}; // the intial speed is in +ve X-axis direction
+
 
     // intialize food
     Rectangle food = {200, 200, GRID_BOX, GRID_BOX};
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
     while (!WindowShouldClose())
     {
         // ---------------------------------------------------------------------------------------------------------
         // Update
         // ---------------------------------------------------------------------------------------------------------
+
         bool collision = 0;
         frame_cnt++;
 
         // The key must be only "pressed" not "down"
         // which means that we only need to tab on the key, not keep it down all the movement duration
         // notice: here we only assign a value (which iss GRID_BOX) to the speed.x or speed.y
-        if (IsKeyPressed(KEY_DOWN))
+
+        // To prevent the 180-deg turn (moves left while it's moving right):
+        //     we check if the key is pressed as well as if the speed is zero
+        //     if KEY_DOWN is pressed and speed.y is zero => move down
+        //     because speed.y is only zero if the head is moving right or left
+        //     so this condition would become false if the head is moving up
+        //     that way we prevent it from moving doen while it's moving up
+
+        if (IsKeyPressed(KEY_DOWN) && (snake[0].speed.y == 0))
         {
             snake[0].speed = (Vector2){0, GRID_BOX};
         }
-        if (IsKeyPressed(KEY_UP))
+        if (IsKeyPressed(KEY_UP) && (snake[0].speed.y == 0))
         {
             snake[0].speed = (Vector2){0, -GRID_BOX};
         }
-        if (IsKeyPressed(KEY_RIGHT))
+        if (IsKeyPressed(KEY_RIGHT) && (snake[0].speed.x == 0))
         {
             snake[0].speed = (Vector2){GRID_BOX, 0};
         }
-        if (IsKeyPressed(KEY_LEFT))
+        if (IsKeyPressed(KEY_LEFT) && (snake[0].speed.x == 0))
         {
             snake[0].speed = (Vector2){-GRID_BOX, 0};
         }
@@ -68,46 +113,106 @@ int main()
         //  why 4? just tried it and saw what's best
         // notice: here, we use the speed assigned above to actually move the snake
 
-        //stores the previous position of the snake in an array
+        // stores the previous position of the snake in an array
         for (int i = 0; i <= length; i++)
         {
             snake_prev_pos[i].x = snake[i].snake_rect.x;
             snake_prev_pos[i].y = snake[i].snake_rect.y;
         }
 
-        //for each 4 frames, make the head move
+        // for each 4 frames, move the snake and position its body segments
         if (frame_cnt % 4 == 0)
         {
             // Actually move the snake with the previously stored speed
-                snake[0].snake_rect.x += snake[0].speed.x;
-                snake[0].snake_rect.y += snake[0].speed.y;
+            snake[0].snake_rect.x += snake[0].speed.x;
+            snake[0].snake_rect.y += snake[0].speed.y;
             for (int i = 1; i <= length; i++)
             {
-                //each segment of the snake body will take the position of its previous one 
+                // each segment of the snake body will take the position of its previous one
                 snake[i].snake_rect.x = snake_prev_pos[i - 1].x;
                 snake[i].snake_rect.y = snake_prev_pos[i - 1].y;
             }
         }
 
-        // For a screen of size 600 x 600:
-        // we need the food to be exactly on a grid box,not any random position
-        // so, we randomly choose a row and a column
-        // the screen width = 600 , the grid box width = 40 , so we have 15 lines
-        // so we randomly choose from (0,14)
-        int random_col = GetRandomValue(0, (screen_width / GRID_BOX) - 1);
-        int random_row = GetRandomValue(0, (screen_height / GRID_BOX) - 1);
 
-        // collision with food
-        collision= CheckCollisionRecs(snake[0].snake_rect,food);
-        if (collision)
+
+
+
+
+
+
+
+
+
+
+        // Randomize food
+        if (!valid_food_pos)
         {
+
+            // For a screen of size 600 x 600:
+            // we need the food to be exactly on a grid box,not any random position
+            // so, we randomly choose a row and a column :
+            //  int random_col = GetRandomValue(0, (screen_width / GRID_BOX) - 1);
+            // int random_row = GetRandomValue(0, (screen_height / GRID_BOX) - 1);
+            // the screen width = 600 , the grid box width = 40 , so we have 15 lines
+            // so we randomly choose from (0,14)
             // After we have randomly choosen a row and a col , turn these value into pixels
             // hence , we multiply by the grid dimension
-            food.x = random_col * GRID_BOX;
-            food.y = random_row * GRID_BOX;
-            collision = 1;
+
+            food.x = (GetRandomValue(0, (screen_width / GRID_BOX) - 1)) * GRID_BOX;
+            food.y = (GetRandomValue(0, (screen_height / GRID_BOX) - 1)) * GRID_BOX;
+            // now food is active because it's where we check for intersection
+
+            // here he checks if the food.x equals the head's x (snake[0]):
+            // if true, generate new coordinates and recheck with the head's x (snake[0])
+            // if false, then check with the next segment of the snake (snake[1])
+            // and so on for the whole length of the snake
+            int i = 0;
+            while (i < length)
+            {
+                if ((food.x == snake[i].snake_rect.x) && (food.y == snake[i].snake_rect.y))
+                {
+                    food.x = (GetRandomValue(0, (screen_width / GRID_BOX) - 1)) * GRID_BOX;
+                    food.y = (GetRandomValue(0, (screen_height / GRID_BOX) - 1)) * GRID_BOX;
+                    i = 0;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            // This became true, because at where we are now,
+            //  we have a good position for food where no intersection with the body occurs
+            valid_food_pos = 1;
+        }
+
+
+
+
+
+
+
+
+
+        // collision with food
+        collision = CheckCollisionRecs(snake[0].snake_rect, food);
+        if (collision)
+        {
+            // false, because we want to check first for a good food position
+            valid_food_pos = 0;
             length++;
         }
+
+
+
+
+
+
+
+
+
+
 
         // ---------------------------------------------------------------------------------------------------------
         // Drawing
@@ -127,21 +232,17 @@ int main()
             DrawLineV((Vector2){i, 0}, (Vector2){i, screen_height}, GRAY);
         }
 
-        
-        if (collision) DrawText("COLLISION",250,0,20,BLACK);
-
         // Draw Player
         DrawRectangleRec(snake[0].snake_rect, BLACK);
         for (int i = 1; i <= length; i++)
         {
-                DrawRectangleRec(snake[i].snake_rect, DARKBLUE);
+            DrawRectangleRec(snake[i].snake_rect, DARKBLUE);
         }
 
         // Draw food
         DrawRectangleRec(food, BLUE);
         DrawText(TextFormat("length %i", length), 5, 5, 20, BLACK);
-       
-        
+
         EndDrawing();
     }
 
